d814da8fdbac79d224f06abd9b607f292b9e400e
Bruno Bacelar Abe <abe2602@gmail.com>
2026-04-12 17:12:57 -0300

    [chore] Create public profile structure

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -16,8 +16,10 @@ import 'package:injectable/injectable.dart' as _i526;
 import 'package:moovie/di/http_di_module.dart' as _i649;
 import 'package:moovie/di/movies_module.dart' as _i993;
 import 'package:moovie/di/profile_module.dart' as _i510;
+import 'package:moovie/di/public_profile_module.dart' as _i777;
 import 'package:movies/movies.dart' as _i987;
 import 'package:profile/profile.dart' as _i16;
+import 'package:public_profile_feature/public_profile_feature.dart' as _i888;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -29,6 +31,7 @@ extension GetItInjectableX on _i174.GetIt {
     final httpDiModule = _$HttpDiModule();
     final moviesModule = _$MoviesModule();
     final profileModule = _$ProfileModule();
+    final publicProfileModule = _$PublicProfileModule();
     gh.singleton<_i494.LocalClient>(() => httpDiModule.localClient);
     gh.singleton<_i361.Dio>(
       () => httpDiModule.backendDio,
@@ -122,6 +125,21 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i16.GetUserFavoriteMovies>(
       () => profileModule.getUserFavoriteMovies(gh<_i16.ProfileRepository>()),
     );
+    gh.lazySingleton<_i888.PublicProfileRemoteDataSource>(
+      () => publicProfileModule.publicProfileRemoteDataSource(
+        gh<_i494.HttpClient>(instanceName: 'tmdb'),
+      ),
+    );
+    gh.lazySingleton<_i888.PublicProfileRepository>(
+      () => publicProfileModule.publicProfileRepository(
+        gh<_i888.PublicProfileRemoteDataSource>(),
+      ),
+    );
+    gh.factory<_i888.GetPublicProfile>(
+      () => publicProfileModule.getPublicProfile(
+        gh<_i888.PublicProfileRepository>(),
+      ),
+    );
     return this;
   }
 }
@@ -131,3 +149,5 @@ class _$HttpDiModule extends _i649.HttpDiModule {}
 class _$MoviesModule extends _i993.MoviesModule {}
 
 class _$ProfileModule extends _i510.ProfileModule {}
+
+class _$PublicProfileModule extends _i777.PublicProfileModule {}
